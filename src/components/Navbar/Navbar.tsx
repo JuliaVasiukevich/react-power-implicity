c1c75c6328685f176ba9d4676327a73bfec4c7a9
JuliaVasiukevich <julliavas@yandex.ru>
2022-08-05 11:37:09 +0300

    refactor(hw-1):refacor path folder assets

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 import "./index.scss";
 
 export const Navbar = () => {
